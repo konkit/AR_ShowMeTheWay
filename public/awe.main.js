b99d4bbf9ca81154fd148adb2203a94335f87eca
Łukasz Tenerowicz <konkit@gmail.com>
2015-12-16 00:19:22 +0100

    Refactoring of awe.main.js

--- public/awe.main.js
+++ public/awe.main.js
@@ -1,5 +1,20 @@
+var markerData = {
+  markers: [
+    {
+      locationId: 'someLocationPlaceholder',
+      markerId: 'someMarkerPlaceholder',
+      directions: [
+        {
+          targetLocationId: 'someLocationPlaceholder',
+          rotation: { x: 0, y: 0, z: 0 }
+        },
+      ]
+    },
+  ]
+}
+
 window.addEventListener('load', function() {
-  
+
   window.awe.init({
     device_type: awe.AUTO_DETECT_DEVICE_TYPE,
     settings: {
@@ -23,54 +38,7 @@ window.addEventListener('load', function() {
             'js/awe.marker_ar.js',
           ],
           success: function() {
-            awe.setup_scene();
-            awe.pois.add({ id:'poi_1', position: { x:0, y:0, z:10000 }, visible: false });
-            awe.projections.add({
-              id:'projection_1',
-              geometry: { shape: 'cube', x:120, y:120, z:120 },
-              material:{ type: 'phong', color: 0xFFFFFF },
-              texture: { path: 'awe_by_buildAR.png' },
-            }, { poi_id: 'poi_1' });
-            awe.events.add([{
-              id: 'ar_tracking_marker',
-              device_types: {
-                pc: 1,
-                android: 1
-              },
-              register: function(handler) {
-                window.addEventListener('ar_tracking_marker', handler, false);
-              },
-              unregister: function(handler) {
-                window.removeEventListener('ar_tracking_marker', handler, false);
-              },
-              handler: function(event) {
-                if (event.detail) {
-                  if (event.detail['64']) { // we are mapping marker #64 to this projection
-                    awe.pois.update({
-                      data: {
-                        visible: true,
-                        position: { x:0, y:0, z:0 },
-                        matrix: event.detail['64'].transform
-                      },
-                      where: {
-                        id: 'poi_1'
-                      }
-                    });
-                  }
-                  else {
-                    awe.pois.update({
-                      data: {
-                        visible: false
-                      },
-                      where: {
-                        id: 'poi_1'
-                      }
-                    });
-                  }
-                  awe.scene_needs_rendering = 1;
-                }
-              }
-            }])
+            initialize();
           },
         },
         {
@@ -83,3 +51,68 @@ window.addEventListener('load', function() {
     }
   });
 });
+
+function initialize() {
+  awe.setup_scene();
+  addPOIs();
+  addProjections();
+  addEvents();
+}
+
+function addPOIs() {
+  awe.pois.add({ id:'poi_1', position: { x:0, y:0, z:10000 }, visible: false });
+}
+
+function addProjections() {
+  awe.projections.add({
+    id:'projection_1',
+    geometry: { shape: 'cube', x: 200, y: 200, z: 200 },
+    material:{ type: 'phong', color: 0xFFFFFF },
+    texture: { path: 'awe_by_buildAR.png' },
+  }, { poi_id: 'poi_1' });
+}
+
+function addEvents() {
+  awe.events.add([{
+    id: 'ar_tracking_marker',
+    device_types: {
+      pc: 1,
+      android: 1
+    },
+    register: function(handler) {
+      window.addEventListener('ar_tracking_marker', handler, false);
+    },
+    unregister: function(handler) {
+      window.removeEventListener('ar_tracking_marker', handler, false);
+    },
+    handler: function(event) {
+      if (event.detail) {
+
+        //TODO: Change this to 
+        if (event.detail['64']) { // we are mapping marker #64 to this projection
+          awe.pois.update({
+            data: {
+              visible: true,
+              position: { x:0, y:0, z:0 },
+              matrix: event.detail['64'].transform
+            },
+            where: {
+              id: 'poi_1'
+            }
+          });
+        }
+        else {
+          awe.pois.update({
+            data: {
+              visible: false
+            },
+            where: {
+              id: 'poi_1'
+            }
+          });
+        }
+        awe.scene_needs_rendering = 1;
+      }
+    }
+  }])
+}
